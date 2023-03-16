61be686497c9d04233d82dcb9fdeeb19ad70167d
Anthony Damico <ajdamico@gmail.com>
2023-03-16 00:42:10 -0400

    ssa

--- tests/setup.R
+++ tests/setup.R
@@ -12,94 +12,127 @@ GET( ssa_url , write_disk( tf ) )
 
 ssa_files <- unzip( tf , exdir = tempdir() )
 
-demographic_fn <- grep( 'demographic' , ssa_files , value = TRUE )
+ssa_fn <- grep( 'demographic' , ssa_files , value = TRUE )
 
 annual_fn <- grep( 'annual' , ssa_files , value = TRUE )
 
-demographic_tbl <- read_sas( demographic_fn )
+ssa_tbl <- read_sas( ssa_fn )
 
 annual_tbl <- read_sas( annual_fn )
 
-demographic_df <- data.frame( demographic_tbl )
+ssa_df <- data.frame( ssa_tbl )
 
 annual_df <- data.frame( annual_tbl )
 
-names( demographic_df ) <- tolower( names( demographic_df ) )
+names( ssa_df ) <- tolower( names( ssa_df ) )
 
 names( annual_df ) <- tolower( names( annual_df ) )
+summed_earnings_5152 <-
+	with( 
+		subset( annual_df , year_earn %in% 1951:1952 ) , 
+		aggregate( annual_earnings , list( id ) , sum )
+	)
+	
+names( summed_earnings_5152 ) <- c( 'id' , 'tot_cov_earn5152' )
+
+summed_earnings_5306 <-
+	with( 
+		subset( annual_df , year_earn > 1952 ) , 
+		aggregate( annual_earnings , list( id ) , sum )
+	)
+	
+names( summed_earnings_5306 ) <- c( 'id' , 'tot_cov_earn5306' )
+
+summed_quarters_5306 <-
+	with( 
+		subset( annual_df , year_earn > 1952 ) , 
+		aggregate( annual_qtrs , list( id ) , sum )
+	)
+
+names( summed_quarters_5306 ) <- c( 'id' , 'qc5306' )
+earnings_2006 <- annual_df[ annual_df[ , 'year_earn' ] == 2006 , c( 'id' , 'annual_earnings' ) ]
+
+names( earnings_2006 ) <- c( 'id' , 'annual_earnings_2006' )
+stopifnot( all( !is.na( ssa_df ) ) )
+
+before_nrow <- nrow( ssa_df )
+
+ssa_df <- merge( ssa_df , summed_earnings_5152 , all.x = TRUE )
+
+ssa_df <- merge( ssa_df , summed_earnings_5306 , all.x = TRUE )
+
+ssa_df <- merge( ssa_df , summed_quarters_5306 , all.x = TRUE )
+
+ssa_df <- merge( ssa_df , earnings_2006 , all.x = TRUE )
+
+ssa_df[ is.na( ssa_df ) ] <- 0
+
+stopifnot( nrow( ssa_df ) == before_nrow )
+
 # ssa_fn <- file.path( path.expand( "~" ) , "SSA" , "this_file.rds" )
 # saveRDS( ssa_df , file = ssa_fn , compress = FALSE )
 # ssa_df <- readRDS( ssa_fn )
 ssa_df <- 
 	transform( 
-		ssa_df , 
+		ssa_df ,
+
+		decade_of_birth = floor( yob / 10 ) * 10 ,
 		
-		mental_disorder = as.numeric( diag %in% 1:2 ) ,
+		sex = factor( sex , levels = 1:2 , labels = c( 'male' , 'female' ) )
 		
-		program_eligibility =
-			factor( 
-				prel , 
-				
-				levels = 0:5 , 
-				
-				labels =
-					c( "Unspecified" ,
-					"Aged individual" ,
-					"Aged spouse" ,
-					"Disabled or blind individual" ,
-					"Disabled or blind spouse" ,
-					"Disabled or blind child" )
-			)
-			
+		tot_cov_earn3706 = ( tot_cov_earn3750 + tot_cov_earn5152 + tot_cov_earn5306 ) ,
+		
+		qc3706 = ( qc3750 + qc5152 + qc5306 ) ,
+		
+		any_earnings_2006 = ( annual_earnings_2006 > 0 )
 	)
-	
 nrow( ssa_df )
 
-table( ssa_df[ , "stat" ] , useNA = "always" )
-mean( ssa_df[ , "fpmt" ] )
+table( ssa_df[ , "sex" ] , useNA = "always" )
+mean( ssa_df[ , "tot_cov_earn3706" ] )
 
 tapply(
-	ssa_df[ , "fpmt" ] ,
-	ssa_df[ , "stat" ] ,
+	ssa_df[ , "tot_cov_earn3706" ] ,
+	ssa_df[ , "sex" ] ,
 	mean 
 )
-prop.table( table( ssa_df[ , "program_eligibility" ] ) )
+prop.table( table( ssa_df[ , "decade_of_birth" ] ) )
 
 prop.table(
-	table( ssa_df[ , c( "program_eligibility" , "stat" ) ] ) ,
+	table( ssa_df[ , c( "decade_of_birth" , "sex" ) ] ) ,
 	margin = 2
 )
-sum( ssa_df[ , "fpmt" ] )
+sum( ssa_df[ , "tot_cov_earn3706" ] )
 
 tapply(
-	ssa_df[ , "fpmt" ] ,
-	ssa_df[ , "stat" ] ,
+	ssa_df[ , "tot_cov_earn3706" ] ,
+	ssa_df[ , "sex" ] ,
 	sum 
 )
-quantile( ssa_df[ , "fpmt" ] , 0.5 )
+quantile( ssa_df[ , "tot_cov_earn3706" ] , 0.5 )
 
 tapply(
-	ssa_df[ , "fpmt" ] ,
-	ssa_df[ , "stat" ] ,
+	ssa_df[ , "tot_cov_earn3706" ] ,
+	ssa_df[ , "sex" ] ,
 	quantile ,
 	0.5 
 )
-sub_ssa_df <- subset( ssa_df , sex == "F" )
-mean( sub_ssa_df[ , "fpmt" ] )
-var( ssa_df[ , "fpmt" ] )
+sub_ssa_df <- subset( ssa_df , qc3706 >= 40 )
+mean( sub_ssa_df[ , "tot_cov_earn3706" ] )
+var( ssa_df[ , "tot_cov_earn3706" ] )
 
 tapply(
-	ssa_df[ , "fpmt" ] ,
-	ssa_df[ , "stat" ] ,
+	ssa_df[ , "tot_cov_earn3706" ] ,
+	ssa_df[ , "sex" ] ,
 	var 
 )
-t.test( fpmt ~ mental_disorder , ssa_df )
-this_table <- table( ssa_df[ , c( "mental_disorder" , "program_eligibility" ) ] )
+t.test( tot_cov_earn3706 ~ any_earnings_2006 , ssa_df )
+this_table <- table( ssa_df[ , c( "any_earnings_2006" , "decade_of_birth" ) ] )
 
 chisq.test( this_table )
 glm_result <- 
 	glm( 
-		fpmt ~ mental_disorder + program_eligibility , 
+		tot_cov_earn3706 ~ any_earnings_2006 + decade_of_birth , 
 		data = ssa_df
 	)
 
@@ -107,8 +140,9 @@ summary( glm_result )
 library(dplyr)
 ssa_tbl <- as_tibble( ssa_df )
 ssa_tbl %>%
-	summarize( mean = mean( fpmt ) )
+	summarize( mean = mean( tot_cov_earn3706 ) )
 
 ssa_tbl %>%
-	group_by( stat ) %>%
-	summarize( mean = mean( fpmt ) )
+	group_by( sex ) %>%
+	summarize( mean = mean( tot_cov_earn3706 ) )
+
